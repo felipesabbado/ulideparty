f4f1bfea68291eee9079e5942bdbea1cdd1f88a6
Felipe-Note <fsabbado@gmail.com>
2022-05-18 09:34:05 +0100

    new lista.php

---
+++ lista.php
@@ -0,0 +1,32 @@
+<!<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title Page -->
+    <title>Ulide Party</title>
+</head>
+<body>
+<header>
+    <?php include 'menu.php' ?>
+</header>
+<footer>
+    <div class="container">
+        <div class="row ">
+            <div class="col-12 text-center">
+                <img src="img/logos/logo-icon-75x75.png" alt="Ulide Party Logo">
+            </div>
+            <div class="col-12 text-center">
+                <h2>Ulide Party</br>O seu portal de diversão noturna.</h2>
+            </div>
+            <div class="col-12 text-center">
+                <p><i class="fi-br-envelope"></i> <strong>support@ulideparty.pt</strong>
+                    <br>&#169 2022 Ulide Party. Todos os direitos reservados.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
