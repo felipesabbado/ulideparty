e9dada826f2b9c6aee3266668564e21b5c5f57d4
Felipe-Note <fsabbado@gmail.com>
2022-05-19 12:56:13 +0100

    lista

--- lista.php
+++ lista.php
@@ -7,11 +7,40 @@
 
     <!-- Title Page -->
     <title>Ulide Party</title>
+
+    <!-- JS -->
+    <script type="application/javascript" src="javascripts/main.js"></script>
 </head>
 <body>
-<header>
-    <?php include 'menu.php' ?>
+<header id="menu">
 </header>
+<main>
+    <section>
+        <div id="rest" class="container-fluid header bg-cover">
+            <div class="row">
+                <div class="col-12 text-center"><h1>Restaurantes</h1></div>
+                <div class="col-12 text-center slogan"><h2>Descobre os melhores restaurantes de Lisboa</h2></div>
+            </div>
+        </div>
+        <div class="curve-divider inverted"></div>
+    </section>
+    <div class="container">
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h3>Nome do local</h3>
+                    </div>
+                    <img class="card-img" src="">
+                    <h4>Tags do local</h4> <h4>Nota <i class="fi-br-star"></i></h4>
+                    <p>Localização e outras infos</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+<div class="footer-curve-divider"><div></div></div>
 <footer>
     <div class="container">
         <div class="row ">
